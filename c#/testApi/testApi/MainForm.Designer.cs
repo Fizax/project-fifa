910611c0f12fcc55d4ab63090a57fa64c829ec1f
Unknown <feyza@keles.nl>
2019-05-06 12:14:03 +0200

    c# Add Teams api

---
+++ c#/testApi/testApi/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace testApi
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(22, 32);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(186, 23);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Haal teams binnen ";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(22, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 494);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.loadButton);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ c#/testApi/testApi/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace testApi
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            System.Net.WebClient downloader = new System.Net.WebClient();
+            string teamJson;
+
+            try
+            {
+                teamJson = downloader.DownloadString("http://localhost/php-Project/project-fifa-php/teamsAPI.php");
+            }
+            catch (System.Net.WebException)
+            {
+                MessageBox.Show("oeps... er is iets mis gegaan ");
+                return;
+            }
+
+            Team[] team = JsonConvert.DeserializeObject<Team[]>(teamJson);
+
+
+            for (int i = 0; i < team.Length; i++)
+            {
+                listBox1.Items.Add(team[i].Name);
+            }
+            
+        }
+    }
+}
